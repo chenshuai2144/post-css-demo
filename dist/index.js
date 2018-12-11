a220cd3654aec0e36479cbc7ed49a5d5678eb903
陈帅 <qixian.cs@outlook.com>
2018-12-11 23:51:48 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,114 @@
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "postcss", "fs-extra", "prettier"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var postcss_1 = __importDefault(require("postcss"));
+    var fs_extra_1 = __importDefault(require("fs-extra"));
+    var prettier_1 = __importDefault(require("prettier"));
+    var css = fs_extra_1.default.readFileSync("./index.qixiancss");
+    var tempMap = {};
+    var fitMap = {
+        fill: function (url) { return [
+            postcss_1.default.decl({
+                prop: "background-size",
+                value: "100% 100%"
+            }),
+            postcss_1.default.decl({
+                prop: "background-image",
+                value: url
+            })
+        ]; },
+        contain: function (url) { return [
+            postcss_1.default.decl({
+                prop: "background-size",
+                value: "contain"
+            }),
+            postcss_1.default.decl({
+                prop: "background-image",
+                value: url
+            })
+        ]; },
+        cover: function (url) { return [
+            postcss_1.default.decl({
+                prop: "background-size",
+                value: "cover"
+            }),
+            postcss_1.default.decl({
+                prop: "background-image",
+                value: url
+            })
+        ]; },
+        fitHeight: function (url) { return [
+            postcss_1.default.decl({
+                prop: "background-size",
+                value: "auto 100%"
+            }),
+            postcss_1.default.decl({
+                prop: "background-image",
+                value: url
+            })
+        ]; },
+        fitWidth: function (url) { return [
+            postcss_1.default.decl({
+                prop: "background-size",
+                value: "100% auto"
+            }),
+            postcss_1.default.decl({
+                prop: "background-image",
+                value: url
+            })
+        ]; }
+    };
+    postcss_1.default()
+        .process(css, {
+        from: "./index.qixiancss"
+    })
+        .then(function (_a) {
+        var root = _a.root;
+        root.walkRules(function (rule) {
+            if (rule.selector && rule.selector.indexOf("temp") > -1) {
+                var tempName = rule.selector.split("-").pop();
+                tempMap[tempName] = rule.nodes.slice();
+                rule.remove();
+                return;
+            }
+            // 如果是fit属性
+            rule.walkDecls(function (decl) {
+                if (decl.prop === "fit") {
+                    var value = decl.value;
+                    var url = value.split(" ")[0];
+                    var fitProps = value.split(" ").pop();
+                    var newDecls = fitMap[fitProps](url);
+                    if (newDecls) {
+                        decl.replaceWith(newDecls);
+                    }
+                    return;
+                }
+                // 如果是模板属性
+                if (decl.prop === "temp") {
+                    var value = decl.value;
+                    var decls = tempMap[value];
+                    var newDecls = decls.map(function (decl) { return decl.clone(); });
+                    if (decls) {
+                        decl.replaceWith(newDecls);
+                    }
+                }
+            });
+        });
+        var code = root.toString();
+        fs_extra_1.default.writeFileSync("./dist/index.css", prettier_1.default.format(code, {
+            parser: "css"
+        }));
+    });
+});
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,104 @@
+import postcss, { Declaration } from "postcss";
+import fs from "fs-extra";
+import prettier from "prettier";
+
+const css = fs.readFileSync("./index.qixiancss");
+
+const tempMap = {};
+
+const fitMap = {
+  fill: url => [
+    postcss.decl({
+      prop: "background-size",
+      value: "100% 100%"
+    }),
+    postcss.decl({
+      prop: "background-image",
+      value: url
+    })
+  ],
+  contain: url => [
+    postcss.decl({
+      prop: "background-size",
+      value: "contain"
+    }),
+    postcss.decl({
+      prop: "background-image",
+      value: url
+    })
+  ],
+  cover: url => [
+    postcss.decl({
+      prop: "background-size",
+      value: "cover"
+    }),
+    postcss.decl({
+      prop: "background-image",
+      value: url
+    })
+  ],
+  fitHeight: url => [
+    postcss.decl({
+      prop: "background-size",
+      value: "auto 100%"
+    }),
+    postcss.decl({
+      prop: "background-image",
+      value: url
+    })
+  ],
+  fitWidth: url => [
+    postcss.decl({
+      prop: "background-size",
+      value: "100% auto"
+    }),
+    postcss.decl({
+      prop: "background-image",
+      value: url
+    })
+  ]
+};
+
+postcss()
+  .process(css, {
+    from: "./index.qixiancss"
+  })
+  .then(({ root }) => {
+    root.walkRules(rule => {
+      if (rule.selector && rule.selector.indexOf("temp") > -1) {
+        const tempName = rule.selector.split("-").pop();
+        tempMap[tempName] = [...rule.nodes];
+        rule.remove();
+        return;
+      }
+      // 如果是fit属性
+      rule.walkDecls(decl => {
+        if (decl.prop === "fit") {
+          const value = decl.value;
+          const url = value.split(" ")[0];
+          const fitProps = value.split(" ").pop();
+          const newDecls = fitMap[fitProps](url) as Declaration[];
+          if (newDecls) {
+            decl.replaceWith(newDecls);
+          }
+          return;
+        }
+        // 如果是模板属性
+        if (decl.prop === "temp") {
+          const value = decl.value;
+          const decls = tempMap[value] as Declaration[];
+          const newDecls = decls.map(decl => decl.clone());
+          if (decls) {
+            decl.replaceWith(newDecls);
+          }
+        }
+      });
+    });
+    const code = root.toString();
+    fs.writeFileSync(
+      "./dist/index.css",
+      prettier.format(code, {
+        parser: "css"
+      })
+    );
+  });
